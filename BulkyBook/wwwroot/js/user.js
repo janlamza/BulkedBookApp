8fbec9e22bf4179144c403be8257fd07388ee78a
jan <janlamza12@gmail.com>
2021-01-30 21:27:57 +0100

    added admin user management

---
+++ BulkyBook/wwwroot/js/user.js
@@ -0,0 +1,66 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": {
+                    id: "id",
+                    lockoutEnd: "LockoutEnd"
+                },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //user is currently locked 
+                        return `
+                    <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px; margin-left:70px">
+                        <i class="fas fa-lock-open"></i> Unlock
+                    </a>
+                    `;
+                    } else {
+                        return `
+                    <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px; margin-left:70px">
+                        <i class="fas fa-lock"></i> Lock
+                    </a>
+                    `;
+                    }
+                   
+                }, "width": "25%"
+            }
+        ]
+    });
+}
+
+function LockUnlock(id) {
+   
+            $.ajax({
+                type: "POST",
+                url: '/Admin/User/LockUnlock',
+                data: JSON.stringify(id),
+                contentType: "application/json",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        
+   
+}
